ced43aec176c1c958ccd8d13aa36f9dd17b18696
Brahm <brahmwongwandanee@gmail.com>
2021-05-06 11:22:10 -0600

    reorganized help ccommand

--- modules/Base.py
+++ modules/Base.py
@@ -10,7 +10,7 @@ class Base(commands.Cog):
     async def help(self, ctx):
         embed = discord.Embed(
             title='Eruchi',
-            description='uh yeah they call me rappa 3000 and ill tell you the story of freddy fazbear pizza',
+            description='uh yeah they call me rappa 3000 and ill tell you the story of freddy fazbear pizza. \n[Invite Eruchi!](https://discord.com/api/oauth2/authorize?client_id=839147906500919306&permissions=8&scope=bot)',
             color=discord.Colour.from_rgb(77, 157, 255)
         ) \
             .set_thumbnail(url='https://i.imgur.com/CRCB3Hr.png') \
@@ -19,9 +19,8 @@ class Base(commands.Cog):
 [Commands](https://github.com/MrBrahm/Eruchi/wiki/Commands)
 [Github](https://github.com/MrBrahm/Eruchi)
 [Support Server](https://discord.gg/QN4KfD4zsc)
-[Invite](https://discord.com/api/oauth2/authorize?client_id=839147906500919306&permissions=8&scope=bot)
             """) \
-            .add_field(name="Servers", value=len(self.bot.guilds)) \
+            .add_field(name="Servers", value=f"{len(self.bot.guilds)}") \
             .add_field(name="Credits", value="Made by Brahm#8516") \
             .set_footer(text=f'ID: {self.bot.user.id}')
         await ctx.send(embed=embed)
