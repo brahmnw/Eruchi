c544f3712fbe5cb9820e1dc6bb5bd2be4c077bd6
Brahm <brahmwongwandanee@gmail.com>
2021-05-06 14:15:52 -0600

    hid all owner commands, include fancy error handler.

--- bot/cogs/Events.py
+++ bot/cogs/Events.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from discord.ext.commands import Cog
+import traceback
 
 class Events(commands.Cog):
 
@@ -12,6 +13,39 @@ class Events(commands.Cog):
         print('Connected to Discord and ready.')
         print(f'Currently in {len(self.bot.guilds)} guilds.')
         await self.bot.change_presence(activity=discord.Game("whats good"))
+        
+    @Cog.listener()
+    async def on_command_error(self, ctx, exception):
+
+        ignore_errors = [
+            commands.CommandNotFound
+        ]
+
+        for error in ignore_errors:
+
+            if isinstance(exception, error):
+                return
+
+        if isinstance(exception, commands.MissingRequiredArgument):
+
+            await ctx.send(
+                embed=discord.Embed(
+                    color=discord.Colour.red()
+                ) \
+                    .set_footer(text="🚫 Your command is missing a required argument.")
+            )
+
+        else:
+
+            print(f"[COMMAND EXCEPTION] {exception}")
+
+            await ctx.send(
+                embed=discord.Embed(
+                    color=discord.Colour.red()
+                ) \
+                    .set_footer(text="🚫 An unknown error has occurred. A report was sent to the command console.")
+            )
+
 
 def setup(bot):
     bot.add_cog(Events(bot))
--- bot/cogs/Owner.py
+++ bot/cogs/Owner.py
@@ -8,27 +8,43 @@ class Owner(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.group(aliases=['o'], hidden=true)
+    @commands.group(aliases=['o'], hidden=True)
     @commands.is_owner()
     async def _owner(self, ctx):
         return
 
-    @owner.command(aliases=['l'])
+    @_owner.command(aliases=['l'])
     async def _load(self, ctx, module):
-        self.bot.load_extension(f"cogs.{module}")
-        await ctx.send(f":hammer: **Loaded** `{module}`!")
 
-    @owner.command(aliases=['rl'])
+        try:
+            self.bot.load_extension(f"cogs.{module}")
+        
+        except Exception as e:
+            print(e)
+            await ctx.send(f":hammer: `{module}` could not be successfully reloaded.")
+
+        else:
+            await ctx.send(f":hammer: **Reloaded** `{module}`!")
+
+    @_owner.command(aliases=['rl'], hidden=True)
     async def _reload(self, ctx, module):
-        self.bot.reload_extension(f"cogs.{module}")
-        await ctx.send(f":hammer: **Reloaded** `{module}`!")
 
-    @owner.command()
+        try:
+            self.bot.reload_extension(f"cogs.{module}")
+        
+        except Exception as e:
+            print(e)
+            await ctx.send(f":hammer: `{module}` could not be successfully reloaded.")
+
+        else:
+            await ctx.send(f":hammer: **Reloaded** `{module}`!")
+
+    @_owner.command(hidden=True)
     async def _presence(self, ctx, *, presence):
         await self.bot.change_presence(activity=discord.Game(presence))
         await ctx.send(f"Successfully changed presence to `{presence}`!")
 
-    @owner.command(aliases=['ss'])
+    @_owner.command(aliases=['ss'], hidden=True)
     async def _start_aiohttp(self, ctx):
         self.bot.session = aiohttp.ClientSession()
         await ctx.send(f"Started `aiohttp.ClientSession()`.")
